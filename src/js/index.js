f719ef3fee58221e6c6b169b17cea0d8bcde02f6
Mikhail Chekov <chekoff@list.ru>
2020-11-01 17:17:20 +0300

    Started 'header' section

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,11 @@ import regeneratorRuntime from "regenerator-runtime";
     document.addEventListener("DOMContentLoaded", ready);
 
     function ready() {
-        
+        //add event to click to header user menu
+        document.querySelector('.user-header__icon').addEventListener('click', (e) => {
+            e.preventDefault();
+            document.querySelector('.user-header__menu').classList.toggle('user-header_active');
+            //TODO: hide to click any place, when open; implement listener on click to menu, and remove it after menu close; 
+        })
     };
 }());
